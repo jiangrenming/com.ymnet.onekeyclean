67ee81e9a89eaccf73e3b47e54393a439e7d4d2e
zhangxupeng <zxp@youmeng.com>
2017-06-26 10:27:15 +0800

    替换QQ_home

--- app/src/main/java/com/ymnet/onekeyclean/cleanmore/fragment/view/RecyclerInfo.java
+++ app/src/main/java/com/ymnet/onekeyclean/cleanmore/fragment/view/RecyclerInfo.java
@@ -11,7 +11,7 @@ import com.ymnet.onekeyclean.R;
 public class RecyclerInfo {
 
     public String[] FunctionTitle = {"手机加速","微信清理","QQ清理","应用卸载","文件清理"};
-    public int[] FunctionDrawable = {R.drawable.rocket_home,R.drawable.wechat_home, R.drawable.qq_home,R.drawable.setting_home, R.drawable.clearfile_home};
+    public int[] FunctionDrawable = {R.drawable.rocket_home,R.drawable.wechat_home, R.drawable.qqicon_home,R.drawable.setting_home, R.drawable.clearfile_home};
     public int[] FunctionDesc = {R.string.used_memory,R.string.wechat_statistics_data_small,R.string.qq_statistics_data_small,R.string.uninstall_app_data,R.string.file_clean};
 
 //    public String[] sizeAndUnit={"0","MB"};
